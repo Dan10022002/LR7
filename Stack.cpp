730bced72ca15af87874b98e0b88c0c7fa393c9e
Dan10022002 <70635329+Dan10022002@users.noreply.github.com>
2020-12-23 21:00:15 +0300

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -3,7 +3,7 @@
 struct Stack_element
 {
     Stack_element* stack_prev = nullptr;
-    int value;
+    int value = -1;
 };
 
 struct Stack_general
@@ -12,94 +12,105 @@ struct Stack_general
     Stack_element* stack_end;
 };
 
-void Constructor (Stack_general& Stack)
+void Constructor(Stack_general& Stack) //1
 {
     Stack.stack_begin = nullptr;
     Stack.stack_end = nullptr;
 }
 
-void Destructor (Stack_general& Stack)
+unsigned int Size(Stack_general& Stack) //3
 {
-    while (Stack.stack_end != nullptr)
+    size_t counter = 0;
+    if (Stack.stack_end == nullptr)
     {
-        Stack_element* stack_time = new Stack_element;
+        return counter;
+    }
+    else
+    {
+        counter += 1;
+        Stack_element* stack_time;
         stack_time = Stack.stack_end;
-        Stack.stack_end = Stack.stack_end -> stack_prev;
+        while (stack_time->stack_prev != nullptr)
+        {
+            stack_time = stack_time->stack_prev;
+            counter += 1;
+        }
+        return counter;
+    }
+}
+
+void Destructor(Stack_general& Stack) //2
+{
+    for (int h = 0; h < Size(Stack); h++)
+    {
+        Stack_element* stack_time;
+        stack_time = Stack.stack_end;
+        Stack.stack_end = Stack.stack_end->stack_prev;
         delete stack_time;
     }
 }
 
-void Push (Stack_general& Stack, Stack_element& Element)
+void Push(Stack_general& Stack, Stack_element& Element) //4
 {
-    if (Stack.stack_begin == nullptr)
+    if (Stack.stack_end == nullptr)
     {
         Stack_element* stack_time = new Stack_element;
-        stack_time -> value = Element.value;
-        Stack.stack_begin = stack_time;
+        stack_time->value = Element.value;
+        stack_time->stack_prev = nullptr;
         Stack.stack_end = stack_time;
-        delete stack_time;
+        Stack.stack_begin = Stack.stack_end;
     }
     else
     {
         Stack_element* stack_time = new Stack_element;
-        stack_time -> value = Element.value;
-        stack_time -> stack_prev = Stack.stack_end;
+        stack_time->value = Element.value;
+        stack_time->stack_prev = Stack.stack_end;
         Stack.stack_end = stack_time;
-        delete stack_time;
     }
 }
 
-Stack_element Pop (Stack_general& Stack)
+int Pop(Stack_general& Stack) //5
 {
-	int t = Stack.stack_end -> value;
-	Stack.stack_end = Stack.stack_end -> stack_prev;
-	return t;
+    int t = Stack.stack_end->value;
+    Stack.stack_end = Stack.stack_end->stack_prev;
+    return t;
 }
 
-size_t Size (const Stack_general& Stack)
+void Print(Stack_general& Stack) //6
 {
-    size_t stack_size = 0;
-    Stack_element* stack_time = new Stack_element;
+    Stack_element* stack_time;
     stack_time = Stack.stack_end;
-    while (stack_time != nullptr)
+    for (int h = 0; h < Size(Stack); h++)
     {
-        stack_time = stack_time -> stack_prev;
-        stack_size += 1;
-    }
-    delete stack_time;
-    return stack_size;
-}
-
-void Print (Stack_general& Stack)
-{
-    while (Stack.stack_end != nullptr)
-    {
-        int s = Stack.stack_end -> value;
-        std::cout << s << " ";
-        Stack.stack_end = Stack.stack_end -> stack_prev;
+        std::cout << stack_time->value << " ";
+        stack_time = stack_time->stack_prev;
     }
 }
 
 int main()
 {
     Stack_general Stack;
-    Constructor (Stack);
+    Constructor(Stack);
     Stack_element Element;
     int n;
     std::cout << "Enter the number of elements: ";
     std::cin >> n;
-    for (int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        std::cout << "Enter the " << (i+1) << " element: ";
+        std::cout << "Enter the " << (i + 1) << " element: ";
         int Element_int;
         std::cin >> Element_int;
         Element.value = Element_int;
-        Push (Stack, Element);
+        Push(Stack, Element);
     }
-    std::cout << "There are " << Size (Stack) << " element in stack.\n";
+    std::cout << "There are " << Size(Stack) << " element in stack.\n";
     std::cout << "Your stack: \n";
-    Print (Stack);
+    Print(Stack);
     std::cout << "\n";
-    Destructor (Stack);
+    std::cout << Pop(Stack);
+    std::cout << "\n";
+    Print(Stack);
+    std::cout << "\n";
+    Destructor(Stack);
     return 0;
 }
